82eabdcf66abf6500b9dd1bd4c7a6d5ed51e8108
Kaktus <daskaktus@gmail.com>
2018-04-03 13:17:14 +0200

    Create info.js

---
+++ info.js
@@ -0,0 +1,7 @@
+{
+    "AUTHOR" : "Kaktus",
+    "INSTALL_MSG" : "Thanks for using Kaktus Cogs. If you have any issues, please pm me on discord at DasKaktus#5299.",
+    "NAME" : "Kaktus Cogs",
+    "SHORT" : "Unofficial cogs by Kaktus",
+    "DESCRIPTION" : "Thanks for using Kaktus Cogs. If you have any issues, please pm me on discord at DasKaktus#5299."
+}
