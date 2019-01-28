c861198c2abe337742fdfbbf8246ff22767f0dae
Kaktus <daskaktus@gmail.com>
2019-01-28 15:51:19 +0100

    Update custcomimproved.py

--- custcomimproved/custcomimproved.py
+++ custcomimproved/custcomimproved.py
@@ -118,7 +118,7 @@ class CustomCommandsImproved:
                 await self.bot.edit_message(message, ret)
             else:
                 if self.cust_commands[server.id][cmd]["isdm"]:
-                    await self.bot.send_message(author, ret)
+                    await self.bot.send_message(message.author, ret)
                 else:
                     await self.bot.send_message(message.channel, ret)
 
