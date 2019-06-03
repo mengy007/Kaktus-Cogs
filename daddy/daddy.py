670900a33d1aa3d10806601562aeb230e109f622
Kaktus <daskaktus@gmail.com>
2019-06-03 14:04:36 +0200

    Create daddy.py

---
+++ daddy/daddy.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+
+class Daddy:
+
+    __author__ = "OGKaktus (OGKaktus#5299)"
+    __version__ = "1.0"
+    
+    def __init__(self, bot):
+        self.bot = bot
+        try:
+            self.settings = dataIO.load_json(SETTINGFILE)
+        except Exception:
+            self.settings =  {}
+            
+    async def replydad(self, message):
+        user = message.author
+        server = message.server
+        content = message.content
+        if(content[:3].lower() == "im ") :
+		    await self.bot.say("Hello " + content[3:] + ", I'm " + self.bot.user.name)
+		elif (content[:4].lower() == "i'm ")
+		    await self.bot.say("Hello " + content[4:] + ", I'm " + self.bot.user.name)
+        
+
+def setup(bot):
+    n = Daddy(bot)
+    bot.add_listener(n.replydad, "on_message")
+    bot.add_cog(n)
