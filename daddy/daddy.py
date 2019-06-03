d8a5249818b78b96249b11daeeed9932c9613536
Kaktus <daskaktus@gmail.com>
2019-06-03 15:27:13 +0200

    Update daddy.py

--- daddy/daddy.py
+++ daddy/daddy.py
@@ -8,9 +8,6 @@ class Daddy:
     __author__ = "OGKaktus (OGKaktus#5299)"
     __version__ = "1.0"
     
-    def __init__(self, bot):
-        self.bot = bot
-            
     async def replydad(self, message):
         user = message.author
         server = message.server
@@ -19,4 +16,3 @@ class Daddy:
             await self.bot.say("Hello " + content[3:] + ", I'm " + self.bot.user.name)
         elif (content[:4].lower() == "i'm ") :
             await self.bot.say("Hello " + content[4:] + ", I'm " + self.bot.user.name)
-
